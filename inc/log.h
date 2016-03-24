caea74799238ce8f24f4f17c260c651823e8bbed
Radoslaw Czerski <r.czerski@samsung.com>
2016-03-24 14:27:25 +0100

    Removing internal macros.
    dlog_print/_E used instead.
    
    Change-Id: I8ff39415f21cfa2ec329bc2478b91d28fcdd5e2c
    Signed-off-by: Radoslaw Czerski <r.czerski@samsung.com>

--- inc/log.h
+++ inc/log.h
@@ -26,28 +26,18 @@
 #undef LOG_TAG
 #define LOG_TAG "TASK_MGR"
 
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
 #if !defined(_D)
-#define _D(fmt, arg...) LOGD(fmt"\n", ##arg)
+#define _D(fmt, arg...) dlog_print(DLOG_DEBUG, LOG_TAG, "%s: %s[%d]\t " #fmt "\n", __FILENAME__, __func__, __LINE__, ##arg)
 #endif
 
 #if !defined(_W)
-#define _W(fmt, arg...) LOGW(fmt"\n", ##arg)
+#define _W(fmt, arg...) dlog_print(DLOG_WARN, LOG_TAG, "%s: %s[%d]\t " #fmt "\n", __FILENAME__, __func__, __LINE__, ##arg)
 #endif
 
 #if !defined(_E)
-#define _E(fmt, arg...) LOGE(fmt"\n", ##arg)
-#endif
-
-#if !defined(_SD)
-#define _SD(fmt, arg...) SECURE_LOGD(fmt"\n", ##arg)
-#endif
-
-#if !defined(_SW)
-#define _SW(fmt, arg...) SECURE_LOGW(fmt"\n", ##arg)
-#endif
-
-#if !defined(_SE)
-#define _SE(fmt, arg...) SECURE_LOGE(fmt"\n", ##arg)
+#define _E(fmt, arg...) dlog_print(DLOG_ERROR, LOG_TAG, "%s: %s[%d]\t " #fmt "\n", __FILENAME__, __func__, __LINE__, ##arg)
 #endif
 
 #define retvm_if_timer(timer, expr, val, fmt, arg...) do { \
